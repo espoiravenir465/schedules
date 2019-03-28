22c3e336450497c636068f4525c598a255a6d5c6
EC2 Default User <ec2-user@ip-172-31-44-250.us-east-2.compute.internal>
2019-03-28 15:40:29 +0000

    commentstablecorrect

--- schedules/database/migrations/2019_03_27_153924_create_comments_table.php
+++ schedules/database/migrations/2019_03_27_153924_create_comments_table.php
@@ -18,8 +18,7 @@ class CreateCommentsTable extends Migration
             $table->increments('comment_id')->unsigned();
             //イベントID (event_id)/INTEGER
             $table->integer('event_id')->unsigned();
-            //しおりID(schedule_id)/INTEGER
-            $table->integer('schedule_id')->unsigned();
+            
             //イベントタイトル(event_title)/VARCHAR(50)
             $table->string('event_title',50);
             //登録日時
@@ -27,9 +26,9 @@ class CreateCommentsTable extends Migration
             
             //外部キー
              $table->engine = 'InnoDB';
-             $table->foreign('schedule_id')
-                    ->references('schedule_id')
-                     ->on('schedules')
+             $table->foreign('event_id')
+                    ->references('event_id')
+                     ->on('events')
                      ->onDelete('cascade');
         });
     }
